2eab26c417910cc4f075578475264bf665daf5db
Shrikanttathe-hub <142688445+Shrikanttathe-hub@users.noreply.github.com>
2025-04-22 13:30:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
 //your JS code here. If required.
-const openModalBtn = document.getElementById('openModal');
-const modalOpen = document.getElementById('modalOpenNew');
-const closeBtn = document.getElementById('close-modalNew');
-
-openModalBtn.onclick = () => {
-	modalOpen.style.display = 'block';
-};
-
-closeBtn.onclick = () => {
-	modalOpen.style.display = 'none;'
-};
-
-
-window.onclick = (event) => {
-	if(event.target === modalOpen){
-		modalOpen.style.display = "none";
-	}
-};
